379f1daed96c177711e07e72a702e4ea6aec3c1f
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2017-02-17 23:38:09 +0100

    Example script which uses this module

---
+++ example/ticker.js
@@ -0,0 +1,12 @@
+const FileTimestampStream = require('file-timestamp-stream')
+
+const stream = new FileTimestampStream({
+  path: './%Y-%m-%dT%H:%M:%S.log',
+  flags: 'a'
+})
+
+setInterval(() => {
+  let date = new Date()
+  stream.write(`tick: ${date}\r\n`)
+  console.log(`Written to ${stream.currentFilename}`)
+}, 800)
